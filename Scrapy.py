e85892128db33e1f4c9cd59fe102c6576f6ebb1b
Andres Rendo <90790428+andresrendo@users.noreply.github.com>
2023-11-26 23:12:04 -0400

    Add files via upload

--- Scrapy.py
+++ Scrapy.py
@@ -11,7 +11,7 @@ uri = "mongodb+srv://juanraque:Jc30213659@cluster0.rnw7ssa.mongodb.net/?retryWri
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-url = "https://celulares.mercadolibre.com.ve/_NoIndex_True"
+url = "https://celulares.mercadolibre.com.ve/"
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
 
 
@@ -35,38 +35,44 @@ while True:
         product_name = soup.find("h1", class_="ui-pdp-title").text
         product_price = float(soup.find("meta", itemprop="price").get("content"))          
         product_location = f.get_location(soup)
-        product_brand = f.get_brand(soup)
-        product_linea = f.get_linea(soup)
-        product_model = f.get_model(soup)
+        # product_brand = f.get_brand(soup)
+        # product_linea = f.get_linea(soup)
+        # product_model = f.get_model(soup)
         product_ventas = f.get_ventas(soup)
         product_recomendacion = f.get_recomendacion(soup)
         product_aniosExp = f.get_aniosExp(soup)
         product_vendidos = f.get_vendidos(soup)
         product_estado = f.get_estado(soup)
         product_vendedor = f.get_vendedor(soup)
+        product_marca = f.get_brand(soup)
 
         
         # Insert data into MongoDB Atlas
-        # client.get_database("test").get_collection("test").insert_one({
-        #    "Nombre": product_name,
-        #    "Precio": product_price,
-        #    "Ubicacion": product_location
-        # })
+        client.get_database("test").get_collection("test").insert_one({
+            "Nombre": product_name,
+            "Precio": product_price,
+            "Ubicacion": product_location,
+            "Marca": product_marca,
+            "Ventas concretadas": product_ventas,
+            "Recomendacion": product_recomendacion,
+            "Años de experiencia": product_aniosExp,
+            "Vendidos": product_vendidos,
+            "Estado": product_estado,
+            "Vendedor": product_vendedor
+        })
 
-        print(f"""
-        Nombre: {product_name}
-        Precio: {product_price}
-        Ubicacion: {product_location}
-        Marca: {product_brand}
-        Linea: {product_linea}
-        Modelo: {product_model}
-        Ventas concretadas: {product_ventas}
-        Recomendacion: {product_recomendacion}
-        Años de experiencia: {product_aniosExp}
-        Vendidos: {product_vendidos}
-        Estado: {product_estado}
-        Vendedor: {product_vendedor}
-        """)
+        # print(f"""
+        # Nombre: {product_name}
+        # Precio: {product_price}
+        # Ubicacion: {product_location}
+        # Marca: {product_marca}
+        # Ventas concretadas: {product_ventas}
+        # Recomendacion: {product_recomendacion}
+        # Años de experiencia: {product_aniosExp}
+        # Vendidos: {product_vendidos}
+        # Estado: {product_estado}
+        # Vendedor: {product_vendedor}
+        # """)
     
 
     if siguiente_enlace:
